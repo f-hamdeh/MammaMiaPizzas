e77ec81644c9f090529e94be2c323a24801220db
FabiHamdeh <fabianahamdeh@gmail.com>
2025-01-05 14:16:28 -0300

    botão de remover add

--- script.js
+++ script.js
@@ -1,97 +1,5 @@
-// Classe pizza. Permite você pegar o preço de acordo com o tamanho da pizza.
-
-class Pizza {
-    constructor(nome, descricao, preco, categoria) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.preco = preco;
-        this.categoria = categoria;
-    }
-    meioSabor() {
-        return this.preco / 2;
-    }
-
-    pegarPreco(tamanho) {
-        switch (tamanho.toLowerCase()) {
-            case 'pequena':
-                return this.preco * 0.8;
-            case 'média':
-                return this.preco;
-            case 'grande':
-                return this.preco * 1.2;
-        }
-    }
-}
-
-// Classe bebida. Permite você pegar o preço.
-
-class Bebida {
-    constructor(nome, preco) {
-        this.nome = nome;
-        this.preco = preco;
-    }
-    pegarPreco() {
-        return this.preco;
-    }
-}
-
-// Classe pedido. Permite você adicionar bebida ou pizza ao pedido e retorna o valor com o valorTotal()
-
-class Pedido {
-    constructor(id) {
-        this.id = id;
-        this.produtos = [];
-    }
-
-    adicionarPizza(pizza, tamanho) {
-        const valor = pizza.pegarPreco(tamanho);
-        this.produtos.push({ item: pizza, tipo: tamanho, preco: valor });
-    }
-
-    adicionarBebida(bebida) {
-        const valor = bebida.pegarPreco();
-        this.produtos.push({ item: bebida, tipo: 'unico', preco: valor });
-    }
-
-    valorTotal() {
-        const total = this.produtos.reduce(
-            (total, produto) => total + produto.preco,
-            0
-        );
-        return total;
-    }
-}
-
-//  ADICIONANDO 3 SABORES DE PIZZA: CALABRESA, PEPPERONI, PORTUGUESA
-
-const pizzaCalabresa = new Pizza(
-    'Calabresa',
-    'Molho de tomate da casa, mozzarela, azeitona preta, calabresa, cebola roxa e orégano.',
-    45,
-    'Salgada'
-);
-
-const pizzaPepperoni = new Pizza(
-    'Pepperoni',
-    'Molho de tomate da casa, queijo, pepperoni. e orégano.',
-    50,
-    'Salgada'
-);
-
-const pizzaPortuguesa = new Pizza(
-    'Portuguesa',
-    'Molho de tomate da casa, mozzarela, azeitona preta, calabresa, cebola roxa e orégano.',
-    47,
-    'Salgada'
-);
-
-//  ADICIONANDO 3 TIPOS DE BEBIDA: COCA, AGUA E MATTE
-
-const bebidaCocaNormal = new Bebida('Coca-cola', 9);
-const bebidaAgua = new Bebida('Água', 6);
-const bebidaMatteNatural = new Bebida('Matte Natural', 8);
-
 // Função que inicializa o carrinho a partir do localStorage
+
 function inicializarCarrinho() {
     // Pega o carrinho do localStorage (caso já tenha algum item armazenado)
     let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
@@ -119,6 +27,17 @@ function adicionarAoCarrinho(item) {
     atualizarResumoPedido(carrinho);
 }
 
+function removerDoCarrinho(index) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+    // Remove o item do carrinho pelo índice
+    carrinho.splice(index, 1);
+
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+    atualizarResumoPedido(carrinho);
+}
+
 // Função que atualiza o resumo do pedido na página
 function atualizarResumoPedido(carrinho) {
     let resumoHTML = '';
@@ -135,7 +54,7 @@ function atualizarResumoPedido(carrinho) {
         carrinho.forEach((item, index) => {
             resumoHTML += `
             <div class='itemDoPedido'>
-                <button class='btn-remover'>X</button>
+                <button class='btn-remover' onclick='removerDoCarrinho(${index})'>X</button>
                 <p>${index + 1}. ${item.tipo} ${item.nome} - R$ ${
                 item.preco
             }</p>
@@ -169,6 +88,10 @@ function abrirCarrinho() {
 
 function fecharCarrinho() {
     carrinhoTab.style.transform = 'translateX(110%)';
+}
+
+function pagamentoRealizado() {
+    alert('Pagamento Efetuado. Pedido sendo preparado!');
 }
 
 // Função para exibir o menu de pizzas e bebidas
